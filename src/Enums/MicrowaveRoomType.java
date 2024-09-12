c1c84fa77a33407696bf3fdc2203f8882dbe6c88
Hyunseung Choi <bocalist@gmail.com>
2024-09-12 13:24:16 -0600

    finished the Microwave class

---
+++ src/Enums/MicrowaveRoomType.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum MicrowaveRoomType
+{
+    Kitchen,
+    WorkSite,
+}
--- src/ProblemDomain/Microwave.java
+++ src/ProblemDomain/Microwave.java
@@ -1,5 +1,7 @@
 package ProblemDomain;
 
+import Enums.MicrowaveRoomType;
+
 public class Microwave extends Appliance {
     public Double capacity;
     public String roomType;
@@ -10,6 +12,34 @@ public class Microwave extends Appliance {
         super(itemNumber, brand, quantity, wattage, color, price);
         this.capacity = capacity;
         this.roomType = roomType;
+    }
+
+    /**
+     * Method to determine the room type of the microwave
+     */
+    public MicrowaveRoomType roomTypeDisplay()
+    {
+        return switch (this.roomType) {
+            case "Kitchen" -> MicrowaveRoomType.Kitchen;
+            case "Worksite" -> MicrowaveRoomType.WorkSite;
+            default -> throw new IllegalStateException("Unexpected value: " + this.roomType);
+        };
+    }
+
+    /**
+     * Method to format all the properties of the microwave for a file
+     */
+    public String FormatForFile()
+    {
+        String commonFormatted = super.FormatForFile();
+
+        // join all the properties with a semicolon
+        return String.join(
+                ";",
+                commonFormatted,
+                capacity.toString(),
+                roomType
+        );
     }
 
     // redefine the toString method
