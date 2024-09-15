2667d9a913f2a8b90763c731263f190da9431924
brokolidev <bocalist@gmail.com>
2024-09-14 18:49:28 -0600

    added sample codes from D2L

---
+++ demo00-HelloKitty/src/utilities/GrabUserName.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import java.util.Scanner;
+
+public class GrabUserName
+{
+
+	public String getTheName()
+	{
+		System.out.println( "What is your name?" );
+		Scanner keyboardInput = new Scanner( System.in );
+		String userName = keyboardInput.nextLine();
+		return userName;
+	}
+
+}
